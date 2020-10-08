c3c9c306b404a0f5ba78566cdbabeaacc9d1620e
Mithran B <32775475+BalaMithran@users.noreply.github.com>
2020-10-08 15:13:25 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: "mongodb://mithran:mithran@ds12226.mlab.com:12226/meanauthapp", //prod
+  database: "mongodb+srv://mithran:mithran@cluster0.85bx8.mongodb.net/Project 0?retryWrites=true&w=majority", //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: "yoursecret",
 };
